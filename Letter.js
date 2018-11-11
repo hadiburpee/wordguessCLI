25184a619a94d8f65844b1b2281bf82f649ada15
hadiburpee <hadiburpee@gmail.com>
2018-11-11 15:21:55 -0800

    working on letter

---
+++ Letter.js
@@ -0,0 +1,21 @@
+//constructor function
+var Letter = function(letter){
+    this.letterChar = letter;
+    this.letterGuess = false;
+    this.returnChar = function(){
+        if(Letter.letterguess = false){
+            letterChar = "_";
+        }
+        else{
+            letterChar = letter;
+        }
+        console.log("\nletterChar: " + letterChar);
+    }
+    this.letterCheck = function(arg1){
+        if(Letter.guess === arg1){
+            console.log("\nYou've already guessed: " + Letter.guess);
+            Letter.letterGuess = true;
+        }
+    }
+
+}
